4b382d440ba902a34e3571b15ff20df3abdcaf8d
Panta <samyog.panta@dxc.com>
2023-06-18 04:30:44 -0500

    Snake game complete

--- InitialProject/Snake Game/Main.py
+++ InitialProject/Snake Game/Main.py
@@ -5,7 +5,6 @@ from snake import Snake
 from food import Food
 from scoreBoard import ScoreBoard
 
-
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -18,10 +17,9 @@ scoreBoard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
-screen.onkey(snake.down,"Down")
+screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-
 
 game_is_on = True
 while game_is_on:
@@ -29,11 +27,23 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreBoard.count()
 
+    # collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreBoard.game_over()
 
-
+    # collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreBoard.game_over()
 
 screen.exitonclick()
